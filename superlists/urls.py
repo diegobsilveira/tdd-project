3098d398fbf9d65d5f56fe7bce4fa6eb86bdb72e
Diego Barros <diegobsilveira@gmail.com>
2021-03-02 14:19:49 -0300

    Testando o envio de email: redirecionando para o home

--- superlists/urls.py
+++ superlists/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from accounts import views as accounts_views
 from lists import views
 
 urlpatterns = [
     url(r'^$', views.home_page, name='home'),
     url(r'^lists/new$', views.new_list, name='new_list'),
     url(r'^lists/(\d+)/$', views.view_list, name='view_list'),
+    url(r'^accounts/send_login_email$', accounts_views.send_login_email, name='send_login_email'),
 ]
